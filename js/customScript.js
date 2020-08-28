41f252ecdd544271f45dcc1b7a8a9bd8deea0f65
Squirtle <tienhoang.pham@bc.fi>
2020-08-28 12:29:21 +0300

    Modify page body font-family property

--- js/customScript.js
+++ js/customScript.js
@@ -1,5 +1,7 @@
 console.log('Hello world from customScript.js');
 
-document.getElementById('slogan').innerText = 'We hate out customers'
+document.getElementById('slogan').innerText = 'We hate out customers';
 
-document.getElementById('all').style.backgroundColor = '#c7c3c3'
+document.getElementById('all').style.backgroundColor = '#c7c3c3';
+
+document.body.style.fontFamily = 'impact';
