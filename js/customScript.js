a3ad7c8299375bc0c32a094466db3cb1b89518b8
Squirtle <tienhoang.pham@bc.fi>
2020-08-28 13:00:35 +0300

    Add styling to create pointer cursor

--- js/customScript.js
+++ js/customScript.js
@@ -7,8 +7,8 @@ document.getElementById('all').style.backgroundColor = '#c7c3c3';
 document.body.style.fontFamily = 'impact';
 
 document.querySelectorAll('#hot .item').forEach(function(anything){
-    anything.style.margin = '10px 20px'
-    anything.style.boxShadow = '2px 2px 10px'
+    anything.style.margin = '10px 20px';
+    anything.style.boxShadow = '2px 2px 10px';
 })
 
 // <li class="nav-item dropdown menu-large">
@@ -18,8 +18,9 @@ document.querySelectorAll('#hot .item').forEach(function(anything){
 var newsletterEl = document.createElement('li');
 newsletterEl.classList = 'nav-item dropdown menu-large';
 
-var navLinkEl = document.createElement('a')
+var navLinkEl = document.createElement('a');
 navLinkEl.classList = 'nav-link';
 navLinkEl.innerText = 'Newsletter';
+navLinkEl.style.cursor = 'pointer';
 newsletterEl.appendChild(navLinkEl);
 document.querySelectorAll('#navigation .navbar-nav.mr-auto')[0].appendChild(newsletterEl);
