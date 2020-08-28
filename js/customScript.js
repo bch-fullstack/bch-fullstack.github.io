4334939c5be09df510d4ecfbe471cc62c8d5a7dd
Squirtle <tienhoang.pham@bc.fi>
2020-08-28 12:55:27 +0300

    Add newsletter link to navigation bar

--- js/customScript.js
+++ js/customScript.js
@@ -10,3 +10,16 @@ document.querySelectorAll('#hot .item').forEach(function(anything){
     anything.style.margin = '10px 20px'
     anything.style.boxShadow = '2px 2px 10px'
 })
+
+// <li class="nav-item dropdown menu-large">
+// <a class="nav-link">Newsletter</a>
+// </li>
+
+var newsletterEl = document.createElement('li');
+newsletterEl.classList = 'nav-item dropdown menu-large';
+
+var navLinkEl = document.createElement('a')
+navLinkEl.classList = 'nav-link';
+navLinkEl.innerText = 'Newsletter';
+newsletterEl.appendChild(navLinkEl);
+document.querySelectorAll('#navigation .navbar-nav.mr-auto')[0].appendChild(newsletterEl);
