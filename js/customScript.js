d56a968cc1afe696f83636cfd0db536b279acd4a
Squirtle <tienhoang.pham@bc.fi>
2020-08-28 12:25:42 +0300

    Change element with id all background color

--- js/customScript.js
+++ js/customScript.js
@@ -1,3 +1,5 @@
 console.log('Hello world from customScript.js');
 
 document.getElementById('slogan').innerText = 'We hate out customers'
+
+document.getElementById('all').style.backgroundColor = '#c7c3c3'
