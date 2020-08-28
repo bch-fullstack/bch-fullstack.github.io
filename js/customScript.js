22f1d773ac3898bd70eb2da0ca15d8787263560f
Squirtle <tienhoang.pham@bc.fi>
2020-08-28 12:42:25 +0300

    Add styling for #hot .item elements

--- js/customScript.js
+++ js/customScript.js
@@ -5,3 +5,8 @@ document.getElementById('slogan').innerText = 'We hate out customers';
 document.getElementById('all').style.backgroundColor = '#c7c3c3';
 
 document.body.style.fontFamily = 'impact';
+
+document.querySelectorAll('#hot .item').forEach(function(anything){
+    anything.style.margin = '10px 20px'
+    anything.style.boxShadow = '2px 2px 10px'
+})
